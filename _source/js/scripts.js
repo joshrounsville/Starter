7a8ebc61326b70cbd5dbfdf101ba58b9ca9a9bb7
Josh Rounsville <joshrounsville@thebriga.de>
2015-12-29 00:09:12 -0800

    first round build

--- _source/js/scripts.js
+++ _source/js/scripts.js
@@ -26,32 +26,6 @@
   }
 
 
-  //////// setup request animation frame shim
-  (function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x ) {
-      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
-
-    if (!window.requestAnimationFrame) {
-      window.requestAnimationFrame = function(callback, element) {
-        var currTime = new Date().getTime();
-        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-        var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-        lastTime = currTime + timeToCall;
-        return id;
-      };
-    }
-
-    if (!window.cancelAnimationFrame) {
-      window.cancelAnimationFrame = function(id) {
-        clearTimeout(id);
-      };
-    }
-  }());
-
 
 
 
@@ -59,14 +33,22 @@
   //////////////////////////////// Functions ////////////////////////////////
 
 
+  var toggleMobileNav = function() {
 
+    if ( body.hasClass('mobile-nav-show') ) {
+      body.removeClass('mobile-nav-show');
+    } else {
+      body.addClass('mobile-nav-show');
+    }
 
-
-  function main() {
-    console.log('Hello, World!');
   }
 
-  main();
+  $('.js--mobile-nav-toggle').on('touchstart click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    toggleMobileNav();
+  });
 
 
 
